549bfd5611bea546c5a778855643e6b4d80bbbba
沈博闻 <bowenshen@chinhiroshis-MacBook.local>
2019-09-11 12:05:38 -0400

    MessageService added

---
+++ src/main/java/com/ascending/training/bowen/service/MessageService.java
@@ -0,0 +1,56 @@
+package com.ascending.training.bowen.service;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.*;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MessageService {
+
+    @Autowired
+    Logger logger;
+    @Autowired
+    AmazonSQS amazonSQS;
+
+    public String createQueue(String queueName){
+        String queueUrl = null;
+
+        try{
+            queueUrl = getQueueUrl(queueName);
+        }
+        catch (QueueDoesNotExistException e){
+            CreateQueueRequest createQueueRequest = new CreateQueueRequest(queueName);
+            queueUrl = amazonSQS.createQueue(createQueueRequest).getQueueUrl();
+        }
+        return queueUrl;
+    }
+
+    public String getQueueUrl(String queueName){
+        return amazonSQS.getQueueUrl(queueName).getQueueUrl();
+    }
+
+    public void sendMessage(String queueName, String msg){
+        Map<String, MessageAttributeValue> messageAttributes = new HashMap();
+        MessageAttributeValue messageAttributeValue = new MessageAttributeValue();
+        messageAttributeValue.withDataType("String").withStringValue("File URL in S3");
+        messageAttributes.put("message", messageAttributeValue);
+        String queueUrl = getQueueUrl(queueName);
+        SendMessageRequest sendMessageRequest = new SendMessageRequest();
+        sendMessageRequest.withQueueUrl(queueUrl)
+                .withMessageBody(msg)
+                .withMessageAttributes(messageAttributes);
+        amazonSQS.sendMessage(sendMessageRequest);
+    }
+
+    public List<Message> getMessage(String queueName){
+        String queueUrl = getQueueUrl(queueName);
+        ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(queueUrl);
+        List<Message> messages = amazonSQS.receiveMessage(receiveMessageRequest).getMessages();
+        return messages;
+    }
+
+}
